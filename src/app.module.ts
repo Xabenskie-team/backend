bd4f6d376be4f9e3602f8134e7537374bbacd9ad
Kostya <lukyanovKR@gmail.com>
2024-02-24 16:52:40 +0300

    schemas prisma update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
